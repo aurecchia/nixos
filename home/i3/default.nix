9cf9c38c92cfdb7351ddac2764d2b2d62b48f0f2
Alessio Aurecchia <alessio@aurecchia.ch>
2024-01-07 15:11:50 +0100

    fix i3

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -11,9 +11,29 @@ in {
    
       config = {
         modifier = mod;
+
+        fonts = {
+          names = [ "JetBrains Mono" ];
+          size = 10.0;
+        };
+        
+        keybindings = {
+          "${mod}+q" = "kill";
+
+          # Launchers
+          "${mod}+space" = "exec rofi -show run";
+          "${mod}+Return" = "exec rofi -show drun -show-icons";
+          "${mod}+b" = "bar mode toggle";
+          "${mod}+c" = "exec find ~/.scripts -executable -type f | dmenu $dmenu_opts | /bin/sh &";
+          "${mod}+e" = "exec emoji-picker";
+          "${mod}+f" = "exec thunar";
+          #"${mod}+g" = "exec ~/.scripts/i3-wm-scripts/nextfind.py $dmenu_opts -l 10";
+          "${mod}+t" = "exec kitty";
+          "${mod}+w" = "exec firefox-dev";
+        };
       };
 
-      extraConfig = builtings.readFile ./config;
+      # extraConfig = builtins.readFile ./config;
     };
 
     initExtra = "xset r rate 160 60";
