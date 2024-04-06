9f4b87878265ea9f1183cd06aa0d0f4b72d67b22
Alessio Aurecchia <alessio@aurecchia.ch>
2024-04-06 21:37:08 +0200

    Extra tmux config

--- home/common/tmux.nix
+++ home/common/tmux.nix
@@ -6,5 +6,13 @@
     plugins = with pkgs.tmuxPlugins; [
       sensible
     ];
+    extraConfig = ''
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
+
+      set-option -g status-style bg=white,fg=cyan
+    '';
   };
 }
